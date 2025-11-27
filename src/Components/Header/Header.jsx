c6550d3012f9d77d86654d0a355055ccc2bb2a35
anikshajol <anikshajol@gmail.com>
2025-11-27 15:31:16 +0600

    desktop and mobile version navbar done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { NavLink } from "react-router";
 import { IoMenu } from "react-icons/io5";
 
 const Header = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
   return (
     <nav>
       {/* mobile section */}
       <section className="menu-icon">
-        <IoMenu />
+        <span
+          onClick={() => {
+            setIsOpenMenu(!isOpenMenu);
+          }}
+          className="icon"
+        >
+          {!isOpenMenu ? <IoMenu /> : "X"}
+        </span>
+
+        {/* responsive navigation container */}
+
+        {isOpenMenu ? (
+          <section className="navigation">
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/details">Details</NavLink>
+              </li>
+            </ul>
+          </section>
+        ) : (
+          ""
+        )}
       </section>
 
       {/* logo section */}
